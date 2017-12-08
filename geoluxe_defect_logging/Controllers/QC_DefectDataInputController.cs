d8b8289b37c1d368cf6ae43da341fa7b216ea037
msongkiet <33480228+msongkiet@users.noreply.github.com>
2017-12-08 13:45:56 +0700

    Initial commit

---
+++ geoluxe_defect_logging/Controllers/QC_DefectDataInputController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using geoluxe_defect_logging.Models;
+
+namespace geoluxe_defect_logging.Controllers
+{
+    public class QC_DefectDataInputController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Index
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(IndexModels models)
+        {
+            string Slab_ID = models.Slab_ID;
+            return RedirectToAction("SlabDetail", "QC_DefectDataInput", models);
+        }
+
+        // GET: Slab_Detail 
+        public ActionResult SlabDetail(IndexModels models)
+        {
+            string Slab_ID = models.Slab_ID;
+            ViewBag.Message = Slab_ID;
+            return View();
+        }
+
+        // POST: Slab_Detail 
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult SlabDetail(FormCollection form)
+        {
+            string Slab_ID = form["Slab_ID"];
+
+            string SlabWidth_1 = form["SlabWidth_1"];
+            string SlabWidth_2 = form["SlabWidth_2"];
+
+            string SlabLength_1 = form["SlabLength_1"];
+            string SlabLength_2 = form["SlabLength_2"];
+
+            string SlabThickness_1 = form["SlabThickness_1"];
+            string SlabThickness_2 = form["SlabThickness_2"];
+            string SlabThickness_3 = form["SlabThickness_3"];
+            string SlabThickness_4 = form["SlabThickness_4"];
+            string SlabThickness_5 = form["SlabThickness_5"];
+            string SlabThickness_6 = form["SlabThickness_6"];
+
+            string Shades = form["Shades"];
+
+            if (ModelState.IsValid)
+            {
+                Response.Write("<script>alert('Pass');</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Fail');</script>");
+            }
+
+            return RedirectToAction("DefectDetail", "QC_DefectDataInput");
+        }
+
+        // GET: DefectDetail
+        public ActionResult DefectDetail()
+        {
+            return View();
+        }
+
+        // GET: DefectDetail
+        public ActionResult DefectList()
+        {
+            return View();
+        }
+
+        // GET: Index
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ geoluxe_defect_logging/Models/QC_DefectDataInputModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace geoluxe_defect_logging.Models
+{
+    public class QC_DefectDataInputModels
+    {
+
+    }
+
+    public class IndexModels
+    {       
+        public string Slab_ID { get; set; }
+    }
+
+    public class SlabDetailModels
+    {
+        public string SlabWidth_1 { get; set; }
+        public string SlabWidth_2 { get; set; }
+        public string SlabLength_1 { get; set; }
+        public string SlabLength_2 { get; set; }
+        public string SlabThickness_1 { get; set; }
+        public string SlabThickness_2 { get; set; }
+        public string SlabThickness_3 { get; set; }
+        public string SlabThickness_4 { get; set; }
+        public string SlabThickness_5 { get; set; }
+        public string SlabThickness_6 { get; set; }
+        public string Shades{ get; set; }
+    }
+
+    public class DefectDetailModels
+    {
+        public string DistX { get; set; }
+        public string DistY { get; set; }
+        public string DefectType { get; set; }
+        public bool Reprocess { get; set; }
+    }
+
+    public class DefectListModels
+    {
+        public bool Status { get; set; }
+    }
+}
