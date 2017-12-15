7ae4510ed3c7de18760cc30d60271d4dd136c91e
msongkiet <33480228+msongkiet@users.noreply.github.com>
2017-12-15 13:25:34 +0700

    2

--- geoluxe_defect_logging/Controllers/QC_DefectDataInputController.cs
+++ geoluxe_defect_logging/Controllers/QC_DefectDataInputController.cs
@@ -18,18 +18,19 @@ namespace geoluxe_defect_logging.Controllers
         // POST: Index
         [HttpPost]
         [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public ActionResult Index(FormCollection form)
+        //[ValidateAntiForgeryToken]
+        public ActionResult Index(SlabIDPartialModels models)
         {
-            string SlabID = form["SlabID"];
-            TempData["SlabID"] = SlabID;
-            return RedirectToAction("SlabDetail", "QC_DefectDataInput", TempData["SlabID"]);
+            string SlabID = models.SlabID;
+            //TempData["SlabID"] = SlabID;
+            //return View();
+            return RedirectToAction("SlabDetail", "QC_DefectDataInput", models);
         }
 
         // GET: Slab Detail
-        public ActionResult SlabDetail()
+        public ActionResult SlabDetail(SlabIDPartialModels models)
         {
-            ViewBag.message = TempData["SlabID"];
+            ViewBag.message = models.SlabID;
             return View();
         }
 
@@ -40,40 +41,40 @@ namespace geoluxe_defect_logging.Controllers
         }
 
 
-        // POST: Slab Detail 
-        [HttpPost]
-        [AllowAnonymous]
-        [ValidateAntiForgeryToken]
-        public ActionResult SlabDetail(FormCollection form)
-        {
-            //string SlabID = form["SlabID"];
+        //// POST: Slab Detail 
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult SlabDetail(FormCollection form)
+        //{
+        //    //string SlabID = form["SlabID"];
 
-            string SlabWidth_1 = form["SlabWidth_1"];
-            string SlabWidth_2 = form["SlabWidth_2"];
+        //    string SlabWidth_1 = form["SlabWidth_1"];
+        //    string SlabWidth_2 = form["SlabWidth_2"];
 
-            //string SlabLength_1 = form["SlabLength_1"];
-            //string SlabLength_2 = form["SlabLength_2"];
+        //    //string SlabLength_1 = form["SlabLength_1"];
+        //    //string SlabLength_2 = form["SlabLength_2"];
 
-            //string SlabThickness_1 = form["SlabThickness_1"];
-            //string SlabThickness_2 = form["SlabThickness_2"];
-            //string SlabThickness_3 = form["SlabThickness_3"];
-            //string SlabThickness_4 = form["SlabThickness_4"];
-            //string SlabThickness_5 = form["SlabThickness_5"];
-            //string SlabThickness_6 = form["SlabThickness_6"];
+        //    //string SlabThickness_1 = form["SlabThickness_1"];
+        //    //string SlabThickness_2 = form["SlabThickness_2"];
+        //    //string SlabThickness_3 = form["SlabThickness_3"];
+        //    //string SlabThickness_4 = form["SlabThickness_4"];
+        //    //string SlabThickness_5 = form["SlabThickness_5"];
+        //    //string SlabThickness_6 = form["SlabThickness_6"];
 
-            //string Shades = form["Shades"];
+        //    //string Shades = form["Shades"];
 
-            //if (ModelState.IsValid)
-            //{
-            //    Response.Write("<script>alert('Pass');</script>");
-            //}
-            //else
-            //{
-            //    Response.Write("<script>alert('Fail');</script>");
-            //}
-            ViewBag.message = "ViewBAG1";
-            return RedirectToAction("Test", "QC_DefectDataInput", ViewBag.message);
-        }
+        //    //if (ModelState.IsValid)
+        //    //{
+        //    //    Response.Write("<script>alert('Pass');</script>");
+        //    //}
+        //    //else
+        //    //{
+        //    //    Response.Write("<script>alert('Fail');</script>");
+        //    //}
+        //    ViewBag.message = "ViewBAG1";
+        //    return RedirectToAction("Test", "QC_DefectDataInput", ViewBag.message);
+        //}
 
 
         // GET : Slab ID Partial View
--- geoluxe_defect_logging/Models/QC_DefectDataInputModels.cs
+++ geoluxe_defect_logging/Models/QC_DefectDataInputModels.cs
@@ -21,29 +21,43 @@ namespace geoluxe_defect_logging.Models
 
     public class SlabDetailPartialModels
     {
+        [Display(Name = "Order No.")]
+        public string OrderNo { get; set; }
+        [Display(Name = "Mat. Type")]
+        public string MatType { get; set; }
+
         public string SlabWidth_1 { get; set; }
         public string SlabWidth_2 { get; set; }
+
         public string SlabLength_1 { get; set; }
         public string SlabLength_2 { get; set; }
+
         public string SlabThickness_1 { get; set; }
         public string SlabThickness_2 { get; set; }
         public string SlabThickness_3 { get; set; }
         public string SlabThickness_4 { get; set; }
         public string SlabThickness_5 { get; set; }
         public string SlabThickness_6 { get; set; }
+
         public string Shades { get; set; }
     }
 
     public class DefectDetailPartialModels
     {
+        [Display(Name = "Dist. X (mm)")]
         public string DistX { get; set; }
+
+        [Display(Name = "Dist. Y (mm)")]
         public string DistY { get; set; }
+
         public string DefectType { get; set; }
+
         public bool Reprocess { get; set; }
     }
 
     public class DefectListPartialModels
     {
+        [Display(Name = "Status")]
         public bool Status { get; set; }
     }
 }
